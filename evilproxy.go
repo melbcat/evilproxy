19d1433e0426ba2c1d082416a9c8b43e1e9ae610
napitek <root@napitek.co>
2016-07-27 18:22:36 +0200

    Dependecies

--- evilproxy.go
+++ evilproxy.go
@@ -4,9 +4,22 @@ package main
 
 import (
 	"github.com/op/go-logging"
-
 	/*"evilproxy/config"*/
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"log"
+	"net"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+	"runtime"
+	"flag"
+	"encoding/json"
+	"encoding/binary"
 )
 
 var log = logging.MustGetLogger("evilproxy")
